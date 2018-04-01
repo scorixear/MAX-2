d181a765b0e053cbf7c0ca1a1e899487fa9265cd
Paul Keller <pk@corno.de>
2018-04-01 19:20:06 +0200

    FinishSetup start

---
+++ src/gui/FinishFrame.java
@@ -0,0 +1,16 @@
+package gui;
+
+import javax.swing.*;
+import java.util.ArrayList;
+
+/**
+ * author: Paul Keller
+ * date: 01.04.2018
+ * version: 1.0
+ */
+public class FinishFrame extends JFrame {
+    FinishFrame(ArrayList<Spieler> spieler)
+    {
+
+    }
+}
