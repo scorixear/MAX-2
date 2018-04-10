4a02adff23203ce20f0bf1742b02295d4f1d7962
ScorixEar <pk@corno.de>
2018-04-10 19:30:21 +0200

    changed comments

--- src/gui/ZufallsBruchGenerator.java
+++ src/gui/ZufallsBruchGenerator.java
@@ -9,10 +9,10 @@ import java.util.ArrayList;
 public class ZufallsBruchGenerator {
 	
 	public static ArrayList<ArrayList<Object>> tabelleFuellen(int laenge, int breite){
-		//f?llt eine Tabelle mit zuf?lligen Zahlen
+		//füllt eine Tabelle mit zufälligen Zahlen
 		ArrayList<ArrayList<Object>> spielbrett= new ArrayList<>();
 		for(int i=0;i<breite;i++) {
-			//f?gt der Tabelle soviele Listen (Spalten) hinzu, wie die Tabelle breit ist
+			//fügt der Tabelle soviele Listen (Spalten) hinzu, wie die Tabelle breit ist
 			spielbrett.add(listeFuellen(laenge));
 		}
 		return spielbrett;
@@ -20,22 +20,22 @@ public class ZufallsBruchGenerator {
 	private static ArrayList<Object> listeFuellen(int laenge){
 		ArrayList<Object> liste= new ArrayList<>();
 		for(int i=0;i<laenge;i++) {
-			//f?gt soviele Elemente hinzu, wie die Liste lang ist
+			//fügt soviele Elemente hinzu, wie die Liste lang ist
 			liste.add(zufallsBruch());
 		}
 		return liste;
 	}
 	private static Fraction zufallsBruch() {
 		//erschafft einen zuf?lligen Bruch, der im Wertebereich von 1 bis 10 liegt
-		//erschafft zuf?llige Zahl zwischen 1 und 99
+		//erschafft zufällige Zahl zwischen 1 und 99
 		int nenner=zufallsZahl(1,99);
-		//erschafft eine zuf?llige Zahl zwischen nenner und dem niedrigeren Wert von nenner*10 und 99
+		//erschafft eine zufällige Zahl zwischen nenner und dem niedrigeren Wert von nenner*10 und 99
 		int zaehler=zufallsZahl(nenner,Math.min(nenner*10, 99));
-		//gibt den Bruch zur?ck
+		//gibt den Bruch zurück
 		return new Fraction(zaehler+"", nenner+"");
 	}
 	private static int zufallsZahl(int min, int max) {
-		//erschafft eine zufllige Zahl zwischen dem angegebenem minimum und maximum
+		//erschafft eine zufällige Zahl zwischen dem angegebenem minimum und maximum
 		return (int) (Math.random()*(max-min+1)+min);
 	}
 }
