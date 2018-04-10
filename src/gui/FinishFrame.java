df58d1137665050ac1077d96aee3da04d757eb21
ScorixEar <pk@corno.de>
2018-04-10 20:52:56 +0200

    refactors windowadapter to a up to date version

--- src/gui/FinishFrame.java
+++ src/gui/FinishFrame.java
@@ -12,18 +12,7 @@ import java.util.ArrayList;
  * version: 1.0
  */
 class FinishFrame extends JFrame {
-    WindowAdapter windowAd=new WindowAdapter() {
-        @Override
-        public void windowClosing(WindowEvent e){
-            disposeFrame();
-        }
-    };
-    FinishFrame(ArrayList<Spieler> spieler)
-    {
-        ArrayList<Spieler> gewinner = testScore(spieler);
-        setUp(gewinner, spieler);
-        addWindowListener(windowAd);
-    }
+
 
     private void setUp(ArrayList<Spieler> gewinner, ArrayList<Spieler> spieler) {
         setTitle("GEWONNEN");
@@ -51,6 +40,7 @@ class FinishFrame extends JFrame {
         p.setBounds(80,10,140,(gewinner.size()+spieler.size())*30);
         setSize(300, (gewinner.size()+spieler.size())*30+50);
         setLocation(Toolkit.getDefaultToolkit().getScreenSize().width/2-getWidth()/2, Toolkit.getDefaultToolkit().getScreenSize().height/2-getHeight()/2);
+        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
         setVisible(true);
     }
 
@@ -72,9 +62,6 @@ class FinishFrame extends JFrame {
         gewinner.add(winner);
        return gewinner;
        //returned den Gewinner in einer Liste
-    }
-    private void disposeFrame(){
-        this.dispose();
     }
 }
 
