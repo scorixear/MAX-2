3b29429514de4b2826506e54b5a3be52a7084d08
Sarenia24 <38049108+Sarenia24@users.noreply.github.com>
2018-04-11 13:31:45 +0200

    Update ArrowButton.java

--- src/gui/ArrowButton.java
+++ src/gui/ArrowButton.java
@@ -14,7 +14,7 @@ import java.awt.event.ActionListener;
 public class ArrowButton extends JDialog{
     /*
        class ArrowButton
-       second Frame for on-screen controls
+       zweites Fenster für Bildschirm-Steuerung
      */
     private GameMechanic myGame;
 
@@ -63,7 +63,7 @@ public class ArrowButton extends JDialog{
         setLocation(Toolkit.getDefaultToolkit().getScreenSize().width/2+200, Toolkit.getDefaultToolkit().getScreenSize().height/2-50);
         setVisible(false);
         //setUndecorated(true);
-        //only set it visible when game starts
+        //wird nur sichtbar gesetzt, wenn das Spiel startet
     }
     public void setArrowVisible(boolean b){
         setVisible(b);
